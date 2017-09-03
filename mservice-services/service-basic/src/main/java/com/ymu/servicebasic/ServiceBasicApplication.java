70f3e525cf9cea59d9d9599ccfcb18c0e9190c17
xinxiamu <932852117@qq.com>
2017-09-03 23:21:01 +0800

    调试

---
+++ mservice-services/service-basic/src/main/java/com/ymu/servicebasic/ServiceBasicApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.servicebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBasicApplication.class, args);
+	}
+}
---
+++ mservice-services/service-sfd/src/main/java/com/ymu/sfd/core/ServiceSfdApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.sfd.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceSfdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceSfdApplication.class, args);
+	}
+}
